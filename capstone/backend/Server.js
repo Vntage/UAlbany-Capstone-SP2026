47bfaac20d8dcbce9a8833f8228db8125907cc7c
Vincent Jiang <vincentvyj@gmail.com>
2026-02-18 16:28:56 -0500

    FirebaseConfig

--- capstone/backend/Server.js
+++ capstone/backend/Server.js
@@ -5,7 +5,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 // TODO: Replace the following with your app's Firebase configuration
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyDYqAQtdQp9tWBryeQaxNztbE8yw2f2djE",
+  authDomain: "capstone-spring2026.firebaseapp.com",
+  projectId: "capstone-spring2026",
+  storageBucket: "capstone-spring2026.firebasestorage.app",
+  messagingSenderId: "695845619715",
+  appId: "1:695845619715:web:c78741cb5bb3aa91faa636",
+  measurementId: "G-Y5R9RRR909"
 };
 
 const app = initializeApp(firebaseConfig);
