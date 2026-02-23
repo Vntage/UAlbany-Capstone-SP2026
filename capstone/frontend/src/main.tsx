a72e66f3db371331e99efe1d67bdf825c428072e
scigeotech <adammeret1@gmail.com>
2026-02-22 20:09:19 -0500

    fix main again
    fix main again, whoops

--- capstone/frontend/src/main.tsx
+++ capstone/frontend/src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
 import App from './App.tsx'
+import Login from './pages/login.tsx'
+import Signup from './pages/signup.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element= {<App/>}></Route>
+        <Route path="/Login" element= {<Login/>}></Route>
+        <Route path="/Signup" element= {<Signup/>}></Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
